65be3eea5a569a889b48264408639bbba428a669
bhurlow <bhurlow@gmail.com>
2015-11-08 12:18:21 -0500

    bash > node

---
+++ export.sh
@@ -0,0 +1,20 @@
+#! /bin/bash 
+
+NAME=$1
+echo "exporting $NAME"
+
+# stay clean
+rm -rf /tmp/$NAME
+
+# save a copy of the machine data
+cp -r $HOME/.docker/machine/machines/$NAME /tmp/$NAME
+
+# stub out the host specific vars
+cat /tmp/$NAME/config.json | sed -e "s:$HOME:{{HOME}}:g" > /tmp/$NAME/config.json
+
+# make a zip
+zip -r -j $NAME.zip /tmp/$NAME
+
+# clean up
+rm -rf /tmp/$NAME
+
